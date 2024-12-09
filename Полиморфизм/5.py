cd5c1318f8f4273b07e816c071937f7ac571849d
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-12-09 10:25:18 +0300

    Create 5.py

---
+++ Полиморфизм/5.py
@@ -0,0 +1,20 @@
+import json
+
+
+# BEGIN
+class DatabaseConfigLoader():
+    def __init__(self, path):
+        self.path_to_config = path
+
+    def load(self, env):
+        filename = f'database.{env}.json'
+        filepath = self.path_to_config / filename
+        raw_config = json.loads(open(filepath).read())
+
+        if 'extend' not in raw_config:
+            return raw_config
+
+        extend = raw_config['extend']
+        rest = {k: v for k, v in raw_config.items() if k != 'extend'}
+        return {**self.load(extend), **rest}
+# END
