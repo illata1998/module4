3b42f83a433d24cd671b93f1aa6685b3898e0fca
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-12-09 10:03:49 +0300

    Create 17_urls.py

---
+++ Разработка на фреймворке Django/17_urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from simple_blog.categories import views
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='categories_index'),
+    path(
+        'create/',
+        views.CategoryCreateView.as_view(),
+        name='categories_create'
+    ),
+    # BEGIN
+    path(
+        '<int:id>/update/',
+        views.CategoryUpdateView.as_view(),
+        name='categories_update'
+    ),
+    # END
+    path('<int:id>/', views.CategoryView.as_view(), name='categories_detail'),
+]
