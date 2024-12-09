5d647b52e966e572934817709ebb76d21e8e5481
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-12-09 10:29:56 +0300

    Create 9_weather_service.py

---
+++ Полиморфизм/9_weather_service.py
@@ -0,0 +1,13 @@
+API_URL = 'http://localhost:8080/api/v2/'
+
+
+class WeatherService():
+# BEGIN
+    def __init__(self, http_client):
+        self.http_client = http_client
+
+    def look_up(self, city_name):
+        url = f'{API_URL}cities/{city_name}'
+        response = self.http_client.get(url)
+        return response.json()
+# END
