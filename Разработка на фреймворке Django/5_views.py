caff84a47f3361ee5bbfe519d36a5540e5178573
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-12-09 09:12:11 +0300

    Create 5_views.py

---
+++ Разработка на фреймворке Django/5_views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+TEAM = [
+    {'name': 'Yoda', 'position': 'CEO'},
+    {'name': 'Obi-Wan Kenobi', 'position': 'Senior Developer'},
+    {'name': 'Anakin Skywalker', 'position': 'Junior Developer'},
+    {'name': 'Jar Jar Binks', 'position': 'Trainee'},
+]
+
+
+def index(request):
+    return render(request, 'index.html')
+
+# BEGIN
+def about(request):
+    return render(
+        request,
+        'about.html',
+        context={'team': TEAM}
+    )
+# END
