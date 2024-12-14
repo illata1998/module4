951bb5ae51cb4fd2c35b1adcbddd9824438e4e44
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-12-14 14:22:05 +0300

    Create 11_HTMLElement.py

---
+++ Погружаясь в классы/11_HTMLElement.py
@@ -0,0 +1,22 @@
+class HTMLElement:
+    def __init__(self):
+        self.body = None
+
+    def set_text_content(self, body):
+        self.body = body
+
+    @classmethod
+    def get_params(cls):
+        return cls._params
+
+    # BEGIN
+    def __str__(self):
+        params = type(self).get_params()
+        open_tag = f"<{params['name']}>"
+        if not params['pair']:
+            return open_tag
+        close_tag = f"</{params['name']}>"
+        body = self.body if self.body else ""
+
+        return f"{open_tag}{body}{close_tag}"
+    # END
